3da51f5949f62fed5e04f6f9d243e44883b0bb29
sahil <sahilmujawar.seti@gmail.com>
2023-05-14 11:43:50 +0530

    [Moin]: created 1 class file demo4

---
+++ src/main/java/sample_git/demo4.java
@@ -0,0 +1,11 @@
+package sample_git;
+
+public class demo4 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("code from team a Moin  ");
+		System.out.println("hi");
+	}
+
+}
